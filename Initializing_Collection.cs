323fae4fc8d023639abe6614c0b5cf29c085a7cd
wnsgur567 <wnsgur567@naver,com>
2020-05-06 13:37:14 +0900

    05-06 1:37

---
+++ Initializing_Collection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using static System.Console;
+
+
+namespace ConsoleApp1
+{
+    class Initializing_Collection
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 123, 456, 789 };
+
+            ArrayList list = new ArrayList(arr);
+            foreach(object item in list)
+            {
+                WriteLine($"ArrayList : {item}");
+            }
+            WriteLine();
+
+            Stack stack = new Stack(arr);
+            foreach (object item in stack)
+                WriteLine($"Stack : {item}");
+            WriteLine();
+
+            Queue queue = new Queue(arr);
+            foreach(object item in queue)
+                WriteLine ($"Queue : {item}");
+            WriteLine();
+
+            ArrayList list2 = new ArrayList() { 11, 22, 33 };
+            foreach (object item in list2)
+                WriteLine($"ArrayList2 : {item}");
+            WriteLine();
+        }
+    }
+}
---
+++ foreach_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class MyEnumerator
+    {
+        int[] numbers = { 1, 2, 3, 4 };
+        public IEnumerator GetEnumerator()
+        {
+            yield return numbers[0];
+            yield return numbers[1];
+            yield return numbers[2];
+            yield break;
+            yield return numbers[3];
+        }
+    }
+
+
+    class foreach가능한_객체
+    {
+        static void Main()
+        {
+            var obj = new MyEnumerator();
+            foreach (int i in obj)
+                Console.WriteLine(i);
+        }
+    }
+}
